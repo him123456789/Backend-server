8c085ffc6927f4a950c765edcbe873c6ceb37328
him123456789 <sabhimanyu809@gmail.com>
2022-07-26 03:21:08 +0530

    added secure = true

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -7,7 +7,8 @@ export const sendToken = (res, user, statusCode, message) => {
       expires: new Date(
         Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
       ),
-      sameSite:'lax'
+      sameSite:'lax',
+      secure:true
     };
   
     const userData = {
