7031d6d80aeeac757b5e44907b8fd3c5fd7b5f32
him123456789 <sabhimanyu809@gmail.com>
2022-07-26 02:53:29 +0530

    added samesite

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -6,6 +6,7 @@ export const sendToken = (res, user, statusCode, message) => {
       expires: new Date(
         Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
       ),
+      sameSite:'strict'
     };
   
     const userData = {
